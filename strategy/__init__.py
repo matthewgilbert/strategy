c82c4ecc70707b971157165ac5a129c6a4c4b1f0
Matthew Gilbert <matthew.gilbert12@gmail.com>
2018-09-11 09:47:04 -0400

    Export calendar module in __init__

--- strategy/__init__.py
+++ strategy/__init__.py
@@ -1,6 +1,7 @@
 from . import strategy
 from . import rebalance
+from . import calendar
 
 from ._version import __version__  # NOQA
 
-__all__ = ['strategy', 'rebalance']
+__all__ = ['strategy', 'rebalance', 'calendar']
